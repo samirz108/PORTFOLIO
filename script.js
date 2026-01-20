436f03dcc08f3af7ccb46e6d8fdbb0a750a75379
samirz108 <sameershrestha11233@gmail.com>
2026-01-20 16:51:01 +0545

    added background image

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+  const icons = [
+    "favicon1.png",
+    "favicon2.png",
+    "favicon3.png"
+  ];
+
+  let index = 0;
+  setInterval(() => {
+    document.getElementById("favicon").href = icons[index];
+    index = (index + 1) % icons.length;
+  }, 500);  // change favicon every 0.5 seconds
+
